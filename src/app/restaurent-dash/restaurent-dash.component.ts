5d8509d76cfd3bd1fa3d3bbdd973a064bc99b987
Neeraj Singh Bhandari <neerajofficia97@gmail.com>
2021-10-20 22:26:00 +0530

    HalfDone

---
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -0,0 +1,65 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurentData } from './restaurent.model';
+
+@Component({
+  selector: 'app-restaurent-dash',
+  templateUrl: './restaurent-dash.component.html',
+  styleUrls: ['./restaurent-dash.component.css']
+})
+export class RestaurentDashComponent implements OnInit {
+
+  formValue!: FormGroup
+  restaurentModelObj: RestaurentData = new RestaurentData;
+  allRestaurentData: any;
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      phone: [''],
+      address: [''],
+      services: ['']
+    })
+    this.getAllData();
+  }
+
+  //Now Subscribing data
+  addResto() {
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.phone = this.formValue.value.phone;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurent(this.restaurentModelObj).subscribe(res => {
+      console.log(res);
+      alert("THANKS! Your Data is Added");
+      this.formValue.reset();
+      this.getAllData(); //Auto Refresh
+    },
+      err => {
+        alert("error");
+      }
+    )
+  }
+
+  //Fetching Data to Dash
+  getAllData() {
+    this.api.getRestaurent().subscribe(res => {
+      this.allRestaurentData = res;
+    })
+  }
+
+  //Delete
+  deleteResto(data: any) {
+    this.api.deleteRestaurent(data.id).subscribe(res => {
+      alert("Restaurent Records Deleted")
+      this.getAllData();
+    })
+  }
+
+}
---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http: HttpClient) { }
+  //create using POST
+  postRestaurent(data: any) {
+    return this._http.post<any>("http://localhost:3000/posts", data).pipe(map((res: any) => {
+      return res;
+    }))
+  }
+
+  //Get Restaurent using GET
+  getRestaurent() {
+    return this._http.get<any>("http://localhost:3000/posts").pipe(map((res: any) => {
+      return res;
+    }))
+  }
+
+  updateRestaurent(id: number, data: any) {
+    return this._http.put<any>("http://localhost:3000/posts" + id, data).pipe(map((res: any) => {
+      return res;
+    }))
+  }
+
+  deleteRestaurent(id: number) {
+    return this._http.delete<any>("http://localhost:3000/posts" + id).pipe(map((res: any) => {
+      return res;
+    }))
+  }
+}
